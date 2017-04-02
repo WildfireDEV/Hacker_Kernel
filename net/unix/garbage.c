f1b424635a7d422ad02a47ef7cdaee9f3ac28c95
WildfireDEV <wildfiredev@gmail.com>
2017-04-02 14:29:25 +1000

    Revert "Linux 3.10.61 to Linux 3.10.96"
    This reverts commit 064f0d585c0a3cbc7d8a24d7b01c62079175aaa6.

--- net/unix/garbage.c
+++ net/unix/garbage.c
@@ -122,7 +122,7 @@ struct sock *unix_get_socket(struct file *filp)
  *	descriptor if it is for an AF_UNIX socket.
  */
 
-void unix_inflight(struct file *fp)
+void unix_inflight(struct user_struct *user, struct file *fp)
 {
 	struct sock *s = unix_get_socket(fp);
 
@@ -130,7 +130,6 @@ void unix_inflight(struct file *fp)
 
 	if (s) {
 		struct unix_sock *u = unix_sk(s);
-
 		if (atomic_long_inc_return(&u->inflight) == 1) {
 			BUG_ON(!list_empty(&u->link));
 			list_add_tail(&u->link, &gc_inflight_list);
@@ -138,13 +137,13 @@ void unix_inflight(struct file *fp)
 			BUG_ON(list_empty(&u->link));
 		}
 		unix_tot_inflight++;
-		spin_unlock(&unix_gc_lock);
+		user->unix_inflight++;
 	}
 	fp->f_cred->user->unix_inflight++;
 	spin_unlock(&unix_gc_lock);
 }
 
-void unix_notinflight(struct file *fp)
+void unix_notinflight(struct user_struct *user, struct file *fp)
 {
 	struct sock *s = unix_get_socket(fp);
 
@@ -152,12 +151,11 @@ void unix_notinflight(struct file *fp)
 
 	if (s) {
 		struct unix_sock *u = unix_sk(s);
-
 		BUG_ON(list_empty(&u->link));
 		if (atomic_long_dec_and_test(&u->inflight))
 			list_del_init(&u->link);
 		unix_tot_inflight--;
-		spin_unlock(&unix_gc_lock);
+		user->unix_inflight--;
 	}
 	fp->f_cred->user->unix_inflight--;
 	spin_unlock(&unix_gc_lock);
